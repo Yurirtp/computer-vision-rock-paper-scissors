77d0f378e3bdfe2d1bc4503a0179d2e4216fe784
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-11-06 23:11:09 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,10 +1,6 @@
-#%%
-import ipykernel
-import pip
-import conda
- 
+#%% 
 import cv2
-from keras_models.h5 import load_model
+from keras.models import load_model
 import numpy as np
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
