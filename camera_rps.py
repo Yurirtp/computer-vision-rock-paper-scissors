85d87caa3d69cfa01962e5f74f15bcfcc30b446b
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-11-13 22:48:30 +0000

    Update camera_rps.py

--- camera_rps.py
+++ camera_rps.py
@@ -2,13 +2,16 @@
 import cv2
 from keras.models import load_model
 import numpy as np
+import time
 
 def get_prediction():
     model = load_model('keras_model.h5')
     cap = cv2.VideoCapture(0)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
-    while True: 
+    print("Get ready")
+    start = time.time()
+    end = start + 5
+    while start < end and True: 
         ret, frame = cap.read()
         resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
         image_np = np.array(resized_frame)
@@ -17,14 +20,13 @@ def get_prediction():
         prediction = model.predict(data)
         cv2.imshow('frame', frame)
         # Press q to close the window
-        print(prediction)
         max_value_index = np.argmax(prediction[0], axis=0)
         choice = {0: "Nothing",  1: "rock", 2: "paper", 3: "scissors"}
         user_choice =  choice[max_value_index]
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        print(user_choice)
-    
+        elif start == end:
+            return user_choice    
     # After the loop release the cap object
     cap.release()
     # Destroy all the windows
