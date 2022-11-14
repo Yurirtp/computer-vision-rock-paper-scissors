c8d683c3a63eb6546bca080684fdca0a3cf5d84d
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-11-14 21:25:20 +0000

    Update camera_rps.py

--- camera_rps.py
+++ camera_rps.py
@@ -1,4 +1,5 @@
 # %%
+import random
 import cv2
 from keras.models import load_model
 import numpy as np
@@ -40,8 +41,46 @@ def get_prediction():
     # Destroy all the windows
 
   
+def get_computer_choice():
+    comp_list = ['rock','paper','scissors']
+    computer_choice = random.choice(comp_list)
+    return computer_choice
 
 
-get_prediction()
+def get_winner(computer_choice, user_choice):
+    print(f"The computer picked {computer_choice}, you picked {user_choice}")
+    if user_choice == computer_choice:
+            result = ("It is a tie!")
+    elif (computer_choice == "rock" and user_choice == "paper") or (computer_choice == "paper" and user_choice == "scissors") or (computer_choice == "scissors" and user_choice == "rock"):
+        result ="You won!"
+    else: result = "You lost" 
+    print(result)   
+
+def play():
+    computer_choice = get_computer_choice()
+    user_choice = get_prediction()
+    outcome = get_winner(computer_choice, user_choice)
+    computer_wins = 0 
+    user_wins = 0 
+    outcome
+    while not (computer_wins == 3 or user_wins ==3):
+        if outcome == "You lost":
+            computer_wins+=1
+        elif outcome == "You won!":
+            user_wins+=1
+        else: 
+            pass    
+
+    if computer_wins == 3 and computer_wins > user_wins:
+        print("Computer is the winner!")
+    elif user_wins == 3 and user_wins > computer_wins:
+        print("Congratulations the User Wins!")
+    else:
+        pass
+    print(f"Final scores are: Computer {computer_wins} VS. You {user_wins}")
+
+   
+
+play()
 
 # %%
